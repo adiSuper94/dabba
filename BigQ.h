7185e8bc4d2f175a7bfc07568a8b664fb0a42764
Aditya Subramanian <aditarun2008@gmail.com>
2022-03-04 09:31:07 -0500

    Project 2 setup

---
+++ BigQ.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <pthread.h>
+#include <iostream>
+#include "Pipe.h"
+#include "File.h"
+#include "Record.h"
+
+using namespace std;
+
+class BigQ {
+
+public:
+
+	BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen);
+	~BigQ ();
+};
+
